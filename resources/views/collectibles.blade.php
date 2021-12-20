2cede7ca86ebc74446aa6f8acef62e40159ee2d8
gbelda <gbelda56@gmail.com>
2021-12-20 20:58:18 +0100

    correct page names

--- resources/views/collectibles.blade.php
+++ resources/views/collectibles.blade.php
@@ -3,5 +3,5 @@
 @section('page_title', 'Characters')
 
 @section('content')
-    <h1 style="text-align: center; padding: 5rem;">CHARACTERS PAGE</h1>
+    <h1 style="text-align: center; padding: 5rem;">COLLECTIBLES PAGE</h1>
 @endsection
