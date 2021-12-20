5ffeb9af439f0e6815078241d28a023690b0ae17
gbelda <gbelda56@gmail.com>
2021-12-20 17:23:07 +0100

    create artists and writers info layout

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -15,7 +15,7 @@
             <h6 class="gallery">VIEW GALLERY</h6>
         </div>
 
-        <div class="main_content d-flex justify-content-between">
+        <div class="main_content d-flex justify-content-between ">
             <div class="comic_description col-8">
                 <h1 class="comic_title">
                     {{ $comic['title'] }}
@@ -51,8 +51,44 @@
             </div>
         </div>
 
+    </div>
+
+    <div class="specifics d-flex justify-content-between">
+
+        <div class="talent col-6">
+
+            <h2>Talent</h2>
+            <div class="art d-flex">
+                <h6 class="col-4">Art by:</h6>
+                <div class="artists col-8">
+                    @foreach ($comic['artists'] as $artist)
+                        <a href="#">{{ $artist }}</a>
+                        @if (!$loop->last)
+                            ,
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+
+            <div class="writer d-flex">
+                <h6 class="col-4">Written by:</h6>
+                <div class="writers col-8">
+                    @foreach ($comic['writers'] as $writer)
+                        <a href="#">{{ $writer }}</a>
+                        @if (!$loop->last)
+                            ,
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+        </div>
 
 
+        <div class="specs col-5">
+            <h2>
+                Specs
+            </h2>
+        </div>
     </div>
 
 
