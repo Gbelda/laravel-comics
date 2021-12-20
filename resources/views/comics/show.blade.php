8ea1446505f8bec8e4178ba5fa666f1518fe2cb0
gbelda <gbelda56@gmail.com>
2021-12-20 17:04:20 +0100

    complete comic description

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -16,26 +16,38 @@
         </div>
 
         <div class="main_content d-flex justify-content-between">
-            <div class="comic_description col-7">
+            <div class="comic_description col-8">
                 <h1 class="comic_title">
                     {{ $comic['title'] }}
                 </h1>
 
-                <div class="info col-4 d-flex">
-                    <div class="status d-flex col-8 justify-content-between align-items-center px-3">
-                        <h6>U.S. Price: <span class="price">{{ $comic['price'] }}</span></h6>
-                        <h6>AVAILABLE</h6>
+                <div class="info">
+                    <div class="price_info  d-flex">
+
+                        <div class="status d-flex col-8 justify-content-between align-items-center px-3">
+                            <h6>U.S. Price: <span class="price">{{ $comic['price'] }}</span></h6>
+                            <h6>AVAILABLE</h6>
+                        </div>
+
+                        <div class="availability col-4 d-flex justify-content-center align-items-center">
+                            <h6>Check Availability&blacktriangledown;</h6>
+                        </div>
+
                     </div>
-                    <div class="availability col-4 d-flex justify-content-center align-items-center">
-                        <h6>Check Availability&blacktriangledown;</h6>
-                    </div>
+
+                    <p class="comic_text">
+                        {{ $comic['description'] }}
+                    </p>
                 </div>
             </div>
 
-            <div class="advert">
-                <h6>
+            <div class="advert col-3">
+                <h6 class="text-end">
                     ADVERTISEMENT
                 </h6>
+                <div class="adv_photo">
+                    <img src="/img/adv.jpg" alt="">
+                </div>
             </div>
         </div>
 
